ad2e44d0cc862304439767e3e818c8bbe70ef2ac
Lei Chen <leighcheng@gmail.com>
2013-12-19 14:09:10 -0800

    -Fixed bug that that cause error due to picture is nil and OtherName is not searched

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,9 +5,9 @@ class Employee < ActiveRecord::Base
 	def self.search(search)
 		if search
 		  if search =~ /\w+\s\w+/i
-		  	where('FirstName LIKE ? AND LastName LIKE ? OR FirstName LIKE ?', "%#{search.split(' ')[0]}%", "%#{search.split(' ')[1]}%", "%#{search}%")     	
+		  	where('FirstName LIKE ? AND LastName LIKE ? OR FirstName LIKE ? OR OtherName LIKE ?', "%#{search.split(' ')[0]}%", "%#{search.split(' ')[1]}%", "%#{search}%",  "%#{search}%")     	
 		  else
-		    where('FirstName LIKE ? OR LastName LIKE ? OR PhoneExtension LIKE ? OR BusinessPhone LIKE ? OR DeskNumber LIKE ? OR Department LIKE ? OR Title LIKE ? OR Cell LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+		    where('FirstName LIKE ? OR LastName LIKE ? OR OtherName LIKE ? OR PhoneExtension LIKE ? OR BusinessPhone LIKE ? OR DeskNumber LIKE ? OR Department LIKE ? OR Title LIKE ? OR Cell LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
 		  end
 		else
 		  scoped
